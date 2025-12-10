bdc96f534090509f7671a8c62401b5faeade5464
Cristian-MG00 <cristian_carp2000@hotmail.com>
2025-12-10 19:21:10 -0300

    Configure el limiter en 5 intentos por cada 15 minutos

--- src/middleware/rateLimitMiddleware.ts
+++ src/middleware/rateLimitMiddleware.ts
@@ -3,11 +3,11 @@ import rateLimit from "express-rate-limit";
 // la funcion necesita un objeto de configuraciones
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, //esto equivale a 15 min en mls. Para que el usuario pueda volver a registrarse,
-  max: 5000, // cantidad de peticiones que podrá hacer antes de la espera,
+  max: 5, // cantidad de peticiones que podrá hacer antes de la espera,
   handler: (req, res, next, options) => {
     res.status(429).json({
       succes: false,
-      error: `Limite alcanzado ${options.max} solicitudes cada ${
+      error: `Limite alcanzado de ${options.max} solicitudes cada ${
         options.windowMs / 1000 / 60
       } minutos.`,
     });
